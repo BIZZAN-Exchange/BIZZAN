5f546702dfdcf32260f5e1988f64b6dd5298f72e
Hevin <390330302@qq.com>
2021-02-23 17:52:06 +0800

    安卓APP开源:初次提交
    Signed-off-by: Hevin <390330302@qq.com>

---
+++ 02_App_Android/app/src/main/java/com/bizzan/config/UrlConfig.java
@@ -0,0 +1,12 @@
+package com.bizzan.config;
+
+public class UrlConfig {
+
+
+    public static String BASE_URL="https://api.bizzan.pro";
+    public static String C2C_URL="api.bizzan.pro";
+    public static String MARKET_URL="api.bizzan.pro";
+    public static String GROUP_URL="api.bizzan.pro";
+    public static String HEART_URL="api.bizzan.pro";
+
+}
